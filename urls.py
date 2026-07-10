b723288887469ed8122f692680ffcb6420169d64
Andrey-Bakhtin <elnutal@yandex.ru>
2026-07-10 22:05:38 +0500

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,7 @@
+class URL:
+    
+    BASE_URL='https://qa-scooter.praktikum-services.ru'
+    COURIER=f"{BASE_URL}/api/v1/courier"
+    LOGIN_COURIER=f"{BASE_URL}/api/v1/courier/login"
+    ORDERS=f"{BASE_URL}/api/v1/orders"
+    
